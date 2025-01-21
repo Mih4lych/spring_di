ac073f190371dc5d83d898b06738a824df4c6e9e
Mih4lych <mih4illl@gmail.com>
2025-01-21 22:04:02 +0300

    Add Spring REST.

---
+++ src/main/java/com/moh4lych/springdi/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.moh4lych.springdi.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/moh4lych/springdi/services/BeerService.java
@@ -0,0 +1,21 @@
+package com.moh4lych.springdi.services;
+
+import com.moh4lych.springdi.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+
+    Beer saveNewBeer(Beer beer);
+
+    void updateBeerById(UUID beerId, Beer beer);
+
+    void deleteBeerById(UUID beerId);
+
+    void patchBeerById(UUID beerId, Beer beer);
+}
